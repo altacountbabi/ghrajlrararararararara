33bda8c5c057c68ebd6608d2635d393b40677e9d
Joe <epicgamer379@hotmail.com>
2022-10-05 13:12:57 +0300

    a

--- libs/utils.lua
+++ libs/utils.lua
@@ -7,6 +7,12 @@ local run = game:GetService('RunService')
 local rs = game:GetService('ReplicatedStorage')
 local lp = plrs.LocalPlayer
 local char = lp.Character or workspace:WaitForChild(lp.Name)
+function loadLib(name) local github = 'https://raw.githubusercontent.com/altacountbabi/ghrajlrararararararara/main/libs/'..name..'.lua' return loadstring(game:HttpGet(github))() end
+local libs = {
+    firing = loadLib('fire'),
+    input = loadLib('input')
+}
+
 
 function lib.tp(cf)
     char:SetPrimaryPartCFrame(cf)
@@ -14,6 +20,39 @@ end
 
 function lib.reset()
     char.Humanoid.Health = 0
+end
+
+function lib.getGlove()
+    return lp:WaitForChild('leaderstats').Glove.Value
+end
+
+function lib.getSlaps()
+    return lp:WaitForChild('leaderstats').Slaps.Value
+end
+
+function lib.canEquipGlove(glove)
+    local stands = workspace:WaitForChild('Lobby'):WaitForChild('GloveStands')
+    local stand = stands[glove]
+    local slaps = tonumber(stand:WaitForChild('SlapsInfoPart'):WaitForChild('SurfaceGui'):WaitForChild('TextLabel').Text)
+    if slaps > lib.getSlaps() then
+        return false
+    else
+        return true
+    end
+end
+
+function lib.setGlove(glove)
+    if lib.canEquipGlove(glove) then
+        libs.firing.click()
+    end
+end
+
+function lib.isAlive()
+    if char.Humanoid.Health == 0 then
+        return false
+    else
+        return true
+    end
 end
 
 
