ca5b15507319f3f0f639b59d8fa923f19b290797
Joe <epicgamer379@hotmail.com>
2022-10-05 13:32:33 +0300

    a

--- main.lua
+++ main.lua
@@ -8,13 +8,14 @@ local lp = srv.plrs.LocalPlayer
 local mouse = lp:GetMouse()
 local char = lp.Character or workspace:WaitForChild(lp.Name)
 
-
-
-
 function loadLib(name)
     local github = 'https://github.com/altacountbabi/ghrajlrararararararara/blob/main/libs/'..name..'.lua'
-    print(github)
+    return loadstring(game:HttpGet(github))()
 end
 
-loadLib('fire')
-loadLib('input')
+
+local libs = {
+    loadLib('fire'),
+    loadLib('input'),
+    loadLib('utils')
+}
